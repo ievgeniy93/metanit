c1033524b5aa55cca9544f07be222c7738a42cd2
ievgeniy93 <63505406+ievgeniy93@users.noreply.github.com>
2020-05-06 20:32:21 +0300

    программа сортировки массива

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,25 +1,41 @@
 ﻿using System;
-
-namespace ConsoleApp1
+namespace SortApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string password;
-            do
+            // ввод чисел
+            int[] nums = new int[7];
+            Console.WriteLine("Введи семь чисел");
+            for (int i = 0; i < nums.Length; i++)
             {
-                Console.WriteLine("Введите пароль");
-                password = Console.ReadLine();
+                Console.Write("{0}-е число: ", i + 1);
+                nums[i] = Int32.Parse(Console.ReadLine());
             }
-            while (password != "root");
+
+            // сортировка
+            int temp;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+            // вывод
+            Console.WriteLine("Вывод отсортированного массива");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+            Console.ReadLine();
         }
     }
-
-
-
-
-
-
 }
-    
