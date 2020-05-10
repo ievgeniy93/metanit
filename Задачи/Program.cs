6e696639a064a77a77bd7e2500df990e1d178058
ievgeniy93 <63505406+ievgeniy93@users.noreply.github.com>
2020-05-10 22:01:28 +0300

    задачка

---
+++ Задачи/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Задачи
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        
+
+            public static byte sum(int a, int b) => Convert.ToByte(a + b);
+
+        
+        
+    }
+}
